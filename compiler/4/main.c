a2deaa5369de0e99a95c4b26b8bce7b5716e0fb2
yang.zhenzhen <soul11201@gmail.com>
2017-04-03 19:07:14 +0800

    copy

--- compiler/4/main.c
+++ compiler/4/main.c
@@ -1,3 +1,17 @@
+/*
+ 在这个题目中，要求你完成一个针对算术表达式的语法分析器。该算术表达式的上下文无关文法是：
+E -> E + T
+   | E - T
+   | T
+T -> T * F
+   | T / F
+   | F
+F -> num
+   | (E)
+请下载我们提供的C代码框架，并把其中缺少的部分补充完整。
+
+ */
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
