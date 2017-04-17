e76a227cff1fc9a4953a89d1a8cea63b9a8b671d
yang.zhenzhen <soul11201@gmail.com>
2017-04-17 17:33:19 +0800

    7

---
+++ compiler/7/semant.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "semant.h"
+
+#define TODO()\
+  do{\
+  printf ("TODO: add your code at file: \"%s\", line: %d\n", __FILE__, __LINE__);\
+}while(0)
+
+//////////////////////////////////////
+// the symbol table
+List_t table = 0;
+
+// lookup an "id" in the symbol table.
+// return "type" on success; and -1 for failure
+Type_t Table_lookup (char *id)
+{
+  List_t p = table;
+  while (p){
+    Dec_t d = (Dec_t)p->data;
+    if (strcmp(d->id, id)==0)
+      return d->type;
+    p = p->next;
+  }
+  return -1;
+}
+
+void Table_insert (Dec_t dec)
+{
+  
+  if (Table_lookup (dec->id) != -1){
+    fprintf (stderr, "Error: the variable "
+	     "\"%s\" has been declared!\n", dec->id);
+    exit (0);
+  }
+  table = List_new (dec, table);
+  return;
+}
+
+//////////////////////////////////////////
+// dec
+void check_dec(Dec_t d)
+{
+  Table_insert (d);
+}
+
+void check_decs(List_t decs)
+{
+  while (decs){
+    Dec_t d = (Dec_t)decs->data;
+    check_dec(d);
+    decs = decs->next;
+  }
+  return;
+}
+
+////////////////////////////////////////
+// exp
+
+// Your job:
+Type_t check_exp (Exp_t exp)
+{
+  TODO();
+}
+
+////////////////////////////////////////
+// stm
+
+// Your job:
+void check_stm (Stm_t stm)
+{
+  TODO();
+}
+
+void check_stms(List_t stms)
+{
+  while (stms){
+    Stm_t s = (Stm_t)stms->data;
+    check_stm(s);
+    stms = stms->next;
+  }
+  return;
+  TODO();
+}
+
+
+void Semant_check(Prog_t prog)
+{
+  // create table
+  check_decs(prog->decs);
+  // check stm
+  check_stms(prog->stms);
+  return;
+}
